1d51253798d6aeaf25ae726644235333281aa5bc
Hansol Kim <89935597+kimh2016@users.noreply.github.com>
2022-02-02 22:27:52 -0500

    Create hello_world.py

---
+++ labs/lab01/hello_world.py
@@ -0,0 +1,10 @@
+"""CS 108 Lab - 1.0 
+
+The module was to copy the template from the zybook
+and follow the instructions, printing Hellow world!
+as an output.
+
+@author: Hansol Kim (hk94)
+@date: Spring, 2021
+"""
+print('Hello, world!')
