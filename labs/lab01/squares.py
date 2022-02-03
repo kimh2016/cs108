f0a911ea3f00767987f2b365627fd311cea0645f
Hansol Kim <89935597+kimh2016@users.noreply.github.com>
2022-02-02 22:28:28 -0500

    Add files via upload

---
+++ labs/lab01/squares.py
@@ -0,0 +1,11 @@
+"""CS 108 Lab 1.1
+
+Execute '12 squared is 144' when Number : 12
+
+@author: Hansol Kim (hk94)
+@date: Spring, 2021
+"""
+
+print('Number:', end=' ')
+Number = int(input())
+print(Number, 'squared is', Number**2)
---
+++ labs/lab01/turtle_star.py
@@ -0,0 +1,23 @@
+"""CS 108 - Lab 1.2
+
+Make a star by listing lines of segments!
+
+@author: Hansol Kim (hk94)
+@date: Spring, 2021
+"""
+
+import turtle
+
+window = turtle.Screen()
+pen = turtle.Turtle()
+
+pen.forward(250)
+pen.right(144)
+pen.forward(250)
+pen.right(144)
+pen.forward(250)
+pen.right(144)
+pen.forward(250)
+pen.right(144)
+pen.forward(250)
+
